ac75273d5235a00459c029b2b84d8114d9c23ff6
Jacob Simpson <jacob.simpson@gmail.com>
2023-09-23 06:10:29 -0700

    Update the verbosity levels.
    Previous implementation of verbose would generate messages from all
    libraries included in the final product, as well as the actual
    executable.
    
    The new implementation should only generate messages from the code for
    this product, until you get to high enough levels of verbosity.

--- src/verbose.rs
+++ src/verbose.rs
@@ -1,23 +1,28 @@
-use tracing_subscriber::filter::LevelFilter;
+use tracing_subscriber::filter::{LevelFilter, Targets};
 use tracing_subscriber::fmt;
 use tracing_subscriber::prelude::*;
 
 pub fn init(verbose_level: u8) {
+    let filter_layer = match verbose_level {
+        0 => Targets::new().with_default(LevelFilter::OFF),
+        1 => Targets::new().with_target("git-github-pull-request", LevelFilter::ERROR),
+        2 => Targets::new().with_target("git-github-pull-request", LevelFilter::WARN),
+        3 => Targets::new().with_target("git-github-pull-request", LevelFilter::INFO),
+        4 => Targets::new().with_target("git-github-pull-request", LevelFilter::DEBUG),
+        5 => Targets::new().with_target("git-github-pull-request", LevelFilter::TRACE),
+        6 => Targets::new().with_default(LevelFilter::ERROR),
+        7 => Targets::new().with_default(LevelFilter::WARN),
+        8 => Targets::new().with_default(LevelFilter::INFO),
+        9 => Targets::new().with_default(LevelFilter::DEBUG),
+        _ => Targets::new().with_default(LevelFilter::TRACE),
+    };
+
     let fmt_layer = fmt::layer()
         .with_level(false)
         .with_target(false)
         .with_thread_ids(false)
         .with_thread_ids(false)
         .compact();
-
-    let filter_layer = match verbose_level {
-        0 => LevelFilter::OFF,
-        1 => LevelFilter::ERROR,
-        2 => LevelFilter::WARN,
-        3 => LevelFilter::INFO,
-        4 => LevelFilter::DEBUG,
-        _ => LevelFilter::TRACE,
-    };
 
     tracing_subscriber::registry()
         .with(filter_layer)

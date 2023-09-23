5a2306839f96b3b9663995a97ef2b0763d4b3e1f
Jacob Simpson <jacob.simpson@gmail.com>
2023-09-23 05:10:53 -0700

    Connect to the local repository and find current checkout.

--- src/create.rs
+++ src/create.rs
@@ -1,5 +1,8 @@
 use anyhow::{anyhow, Result};
+use git2::Repository;
 
 pub async fn create_pull_request() -> Result<()> {
+    let _repo = Repository::discover(".")?;
+
     Err(anyhow!("Nothing to see here."))
 }

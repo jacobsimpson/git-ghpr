62a7513629bff811ed464720b947e3af340ef593
Jacob Simpson <jacob.simpson@gmail.com>
2023-09-23 05:04:57 -0700

    Add the outline of the create command.
    Just the basic structure, it doesn't do anything yet.

---
+++ src/create.rs
@@ -0,0 +1,5 @@
+use anyhow::{anyhow, Result};
+
+pub async fn create_pull_request() -> Result<()> {
+    Err(anyhow!("Nothing to see here."))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
+use crate::configuration::Commands;
 use anyhow::Result;
 use clap::Parser;
 
 mod configuration;
+mod create;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let _options = configuration::Options::parse();
-    Ok(())
+    let options = configuration::Options::parse();
+
+    match options.command {
+        Commands::Create { branch_prefix: _ } => create::create_pull_request().await,
+    }
 }

8b2c72b0e18adb88f846f66703b8a35f9f8881b2
Jacob Simpson <jacob.simpson@gmail.com>
2023-09-23 15:43:48 -0700

    Creates a new branch if there isn't one.

--- src/create.rs
+++ src/create.rs
@@ -20,10 +20,7 @@ pub async fn create_pull_request() -> Result<Message> {
         Some(b) => b,
         None => {
             info!("No existing branch, creating a new one.");
-            return Err(Error::UnableToCreateBranch {
-                branch_name: "not-yet".to_string(),
-                base_commit: current_commit.id().to_string(),
-            });
+            create_new_branch(&repo, &current_commit, branch_prefix)?
         }
     };
 
@@ -49,4 +46,58 @@ fn get_branch_for_commit<'a>(
     }
 
     Ok(None)
+}
+
+fn create_new_branch<'a>(
+    repo: &'a Repository,
+    commit: &Commit<'a>,
+    branch_prefix: Option<String>,
+) -> Result<Branch<'a>, git2::Error> {
+    // Get commit message summary.
+    let summary = match commit.summary() {
+        Some(m) => m,
+        None => {
+            return Err(git2::Error::from_str(
+                "Could not get the commit message summary for this commit.",
+            ))
+        }
+    };
+
+    // Transform commit message.
+    let mut branch_name = transform(summary);
+
+    if let Some(branch_prefix) = branch_prefix {
+        branch_name = format!(
+            "{prefix}-{suffix}",
+            prefix = branch_prefix,
+            suffix = branch_name
+        );
+    }
+
+    // Create branch.
+    repo.branch(&branch_name, commit, false)
+}
+
+/// Transform the input string into something that is a valid branch name.
+fn transform(summary: &str) -> String {
+    // Currently this filters out non-alphabetic characters. A decision made for
+    // rapid implementation that discriminates against non-English languages. A
+    // better filter would be one based on what characters git and Github don't
+    // allow in branch names.
+    summary
+        .to_lowercase()
+        .chars()
+        .filter(|c| {
+            ('a' <= *c && *c <= 'z')
+                || ('A' <= *c && *c <= 'Z')
+                || *c == '-'
+                || *c == ' '
+        })
+        .map(|c| if c.is_whitespace() { '-' } else { c })
+        // I didn't find any documented limits on Git branch names. I didn't
+        // look for documented limits on Github branch names. I did decide there
+        // is a practical limit for usability, however I don't know what it is,
+        // so this number is arbitrary.
+        .take(40)
+        .collect()
 }
